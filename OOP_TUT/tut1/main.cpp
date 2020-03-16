4509a37cb0a9892691e029d4296a09b03568e76f
mojtaba rostami <mrbilandi@gamil.com>
2020-03-17 00:30:09 +0330

    how to use a private function in deconstructor

--- OOP_TUT/tut1/main.cpp
+++ OOP_TUT/tut1/main.cpp
@@ -7,51 +7,30 @@ class M
 {
 private:
     int x,y,z;
+    int area();
 public:
-    M();
     M(int,int,int);
-    M(M &);
     ~M();
 };
 
-M::M()
-{
-    x=2;y=3;z=4;
-}
-
 M::M(int w, int h, int l):x(w),y(h),z(l)
 {}
-
-M::M(M &m)
-{
-    x=m.x; y=m.y; z=m.z;
+int M::area(){
+    return x*y*z;
 }
-
 M::~M()
 {
-    cout<< "volume = "<< x*y*z<< endl;
+    cout<< "volume = "<< M::area()<< endl;
 }
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    // default constructor
-    {
-        cout << "default constructor : ";
-        M cub1;
-    }
 
     // parametric constructor
     {
         cout << "parametric constructor : ";
-        M cub2(3,5,7);
-    }
-
-    // copy constructor
-    {
-        cout << "copy constructor : ";
-        M cub(7,8,5);
-        M cub3(cub);
+        M cub(3,5,7);
     }
 
     return a.exec();
